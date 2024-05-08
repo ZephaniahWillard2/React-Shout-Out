d54cb147e2a7b57ff9dd5b26dc174102661d13be
Zephaniah-Willard <zepiwillard@gmail.com>
2024-05-08 10:40:21 -0500

    added styles to feature-branch-one

--- react-shout-out/src/App.js
+++ react-shout-out/src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [name, setName] = useState('');
   const [date, setDate] = useState('');
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const newShoutOut = {
@@ -21,26 +22,58 @@ function App() {
 
   };
 
-  const styleObject = '';
+  
+
+  const appStyleObject = {
+    textAlign: 'center',
+    fontFamily: 'cambria',
+    backgroundColor: 'rgb(117, 169, 143)',
+    paddingBottom: '20px'
+  };
 
 
+  const headStyleObject = {
+    backgroundColor: '#D4F1F4',
+    padding: '50px'
+  };
+
+
+  const messageStyleObject = {
+    border: 'solid 2px black',
+    borderRadius: '25px',
+    width: '800px',
+    height: '200px',
+    margin: 'auto auto 30px auto',
+    padding: '25px',
+    fontSize: 'large',
+    textAlign: 'left',
+    backgroundColor: 'white',
+    boxShadow: '2px 2px 3px black, -2px -2px 3px black'
+
+  };
+
+
+   
   return (
-    <div className="App">
-
-      <div className='Display-Message'>
-          <h1>Your Shout-Out</h1>
+    <div className="App" style={appStyleObject}>
+      <h1 style={headStyleObject}>Your Shout-Out</h1>
+      <div className='Display-Message' style={messageStyleObject}>
           {shoutOut.length > 0 && 
            shoutOut[0].message}
       </div>
+      
+      <form onSubmit={handleSubmit} 
+      className='Form' 
+      style={messageStyleObject}> 
 
-      <form onSubmit={handleSubmit} className='Form'> 
         <div>
             <label htmlFor='your-message'>Shout-Out </label>
             <input id='your-message' 
                 type='text' 
                 value={message} 
                 onChange={(e) => setMessage(e.target.value)} 
-                placeholder='Shout Out'></input>
+                placeholder='Shout Out'>
+            </input>
         </div>
 
         <div>
